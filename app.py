acb4df0563117420d7e388220c1e97b717cedb1a
Sarah <sjbrooks305@gmail.com>
2020-03-27 18:37:33 -0400

    finished create pet form

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 """Flask app for adopt app."""
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 from flask_debugtoolbar import DebugToolbarExtension
 
 from models import db, connect_db, Pet
 from flask_wtf import FlaskForm
+from forms import AddPetForm
 
 app = Flask(__name__)
 
@@ -29,3 +30,28 @@ def home_page():
     pets = Pet.query.all()
     return render_template("home-page.html", pets=pets)
 
+@app.route("/add", methods=["GET", "POST"])
+def add_pet_form():
+
+    form = AddPetForm()
+
+    if form.validate_on_submit():
+        name = form.name.data
+        species = form.species.data
+        photo_url = form.photo_url.data
+        age = form.age.data
+        notes = form.notes.data
+
+        pet = Pet(name=name, 
+                  species=species, 
+                  photo_url=photo_url, 
+                  age=age, 
+                  notes=notes)
+        
+        db.session.add(pet)
+        db.session.commit()
+
+        return redirect("/add")
+
+    else:
+        return render_template("add-pet-form.html", form=form)
--- forms.py
+++ forms.py
@@ -1,1 +1,17 @@
 """Forms for adopt app."""
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField
+
+
+class AddPetForm(FlaskForm):
+    """Form for adding a pet."""
+
+    name = StringField("Pet name")
+    species = StringField("Species")
+    photo_url = StringField("Photo URL")
+    age = SelectField("Age",
+                      choices=[("baby", "Baby"), 
+                               ("young", "Young"), 
+                               ("adult", "Adult"), 
+                               ("senior", "Senior")])
+    notes = StringField("Notes")
